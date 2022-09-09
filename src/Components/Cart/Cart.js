5f3fa697067603004799fb06c772d47f0caa415b
vedatozayar <vedatozayar@gmail.com>
2022-09-09 21:35:51 +0300

    added validity

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import CartItem from './CartItem';
 
 import styles from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-contex';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `${cartCtx.totalAmount.toFixed(2)}€`;
@@ -17,6 +19,10 @@ const Cart = (props) => {
 
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
   };
 
   const cartItems = (
@@ -34,6 +40,19 @@ const Cart = (props) => {
     </ul>
   );
 
+  const modalActions = (
+    <div className={styles.actions}>
+      <button className={styles['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={styles.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -41,12 +60,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={styles.actions}>
-        <button className={styles['button--alt']} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={styles.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {!isCheckout && modalActions}
     </Modal>
   );
 };
---
+++ src/Components/Cart/Checkout.js
@@ -0,0 +1,99 @@
+import { useRef, useState } from 'react';
+import styles from './Checkout.module.css';
+
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = (value) => value.trim().length === 5;
+
+const Checkout = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredPostalCodeIsValid = !isFiveChars(enteredPostalCode);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+
+    setFormInputsValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalCodeIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+    //submit the cart data
+  };
+
+  const nameControlStyles = `${styles.control} ${
+    formInputsValidity.name ? '' : styles.invalid
+  }`;
+  const streetControlStyles = `${styles.control} ${
+    formInputsValidity.street ? '' : styles.invalid
+  }`;
+  const postalCodeControlStyles = `${styles.control} ${
+    formInputsValidity.postalCode ? '' : styles.invalid
+  }`;
+  const cityControlStyles = `${styles.control} ${
+    formInputsValidity.city ? '' : styles.invalid
+  }`;
+
+  return (
+    <form className={styles.form} onSubmit={confirmHandler}>
+      <div className={nameControlStyles}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' ref={nameInputRef}></input>
+        {!formInputsValidity.name && <p>Please enter a valid name.</p>}
+      </div>
+      <div className={streetControlStyles}>
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' ref={streetInputRef}></input>
+        {!formInputsValidity.street && <p>Please enter a valid street.</p>}
+      </div>
+      <div className={postalCodeControlStyles}>
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' ref={postalCodeInputRef}></input>
+        {!formInputsValidity.postalCode && (
+          <p>Please enter a valid postal Code(5 characters long).</p>
+        )}
+      </div>
+      <div className={cityControlStyles}>
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city' ref={cityInputRef}></input>
+        {!formInputsValidity.city && <p>Please enter a valid city.</p>}
+      </div>
+      <div className={styles.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={styles.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
